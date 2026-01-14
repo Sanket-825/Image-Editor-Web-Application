74886600f75a9ffb6b2558b6d515873b8435e45d
Sanket Parab <sanketparab59@gmail.com>
2026-01-15 01:17:06 +0530

    updated code

---
+++ app.js
@@ -0,0 +1,94 @@
+
+const filters={
+    brightness : {
+        value : 100,
+        min : 0,
+        max : 200
+    },
+    contrast :  {
+        value : 100,
+        min : 0,
+        max : 200,
+        unit : "%"
+    },
+    exposure :  {
+        value : 100,
+        min : 0,
+        max : 200,
+        unit : "%"
+    },
+    saturation :  {
+        value : 100,
+        min : 0,
+        max : 200,
+        unit : "%"
+    },
+    hueRotation :  {
+        value : 0,
+        min : 0,
+        max : 360,
+        unit : "deg"
+    },
+    blur :  {
+        value : 0,
+        min : 0,
+        max : 20,
+        unit : "px"
+    },
+    grayscale : {
+        value : 0,
+        min : 0,
+        max : 100,
+        unit : "%"
+    },
+    sepia : {
+        value : 0,
+        min : 0,
+        max : 100,
+        unit : "%"
+    },
+    opacity : {
+        value : 100,
+        min : 0,
+        max : 100,
+        unit : "%"
+    },
+    invert : {
+        value : 0,
+        min : 0,
+        max : 100,
+        unit : "%"
+    },
+}
+
+const filtersContainer = document.querySelector(".filters-container");
+
+function createFilterElement(name, unit = "%", value, min, max){
+    const div = document.createElement("div");
+    div.classList.add("filter")
+
+    const input = document.createElement("input");
+    input.type = "range"
+    input.min = min
+    input.max = max
+    input.value = value
+    input.id = name
+
+    const p = document.createElement("p");
+    p.innerText = name
+
+    div.appendChild(p)
+    div.appendChild(input)
+
+    return div
+}
+
+Object.keys(filters).forEach(key =>{
+    // console.log(key, filters[key])
+
+    const filterElement = createFilterElement(key, filters[key].unit, filters[key].value, filters[key].min, filters[key].max)
+    // console.log(filterElement)
+
+    filtersContainer.appendChild(filterElement);
+
+})
