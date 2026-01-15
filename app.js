df7054df70fe30b90e0bfea5d584b9fb63751bab
Sanket Parab <sanketparab59@gmail.com>
2026-01-15 20:34:52 +0530

    updated code

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 
-const filters={
+let filters={
     brightness : {
-        value : 100,
-        min : 0,
-        max : 200
-    },
-    contrast :  {
         value : 100,
         min : 0,
         max : 200,
         unit : "%"
     },
-    exposure :  {
+    contrast :  {
         value : 100,
         min : 0,
         max : 200,
@@ -41,7 +36,7 @@ const filters={
         max : 100,
         unit : "%"
     },
-    sepia : {
+    sepia : { 
         value : 0,
         min : 0,
         max : 100,
@@ -60,6 +55,16 @@ const filters={
         unit : "%"
     },
 }
+
+const imageCanvas = document.querySelector("#image-canvas");
+const imageInput = document.querySelector("#image-input");
+const canvasCtx = imageCanvas.getContext("2d");
+const resetBtn = document.querySelector("#reset-btn");
+const downloadBtn = document.querySelector("#download-btn");
+const presetsContainer = document.querySelector(".presets");
+let file = null;
+let image = null;
+let imageNotSelectedWarned = false;
 
 const filtersContainer = document.querySelector(".filters-container");
 
@@ -80,15 +85,161 @@ function createFilterElement(name, unit = "%", value, min, max){
     div.appendChild(p)
     div.appendChild(input)
 
+    input.addEventListener("input", (e)=>{
+        // console.log(e.target.value);
+        filters[name].value = e.target.value;
+        // console.log(name, filters[name]);
+        applyFilters();        
+    })
+
     return div
 }
 
-Object.keys(filters).forEach(key =>{
-    // console.log(key, filters[key])
+function createFilters(){    
+    Object.keys(filters).forEach(key =>{
+        // console.log(key, filters[key])
 
-    const filterElement = createFilterElement(key, filters[key].unit, filters[key].value, filters[key].min, filters[key].max)
-    // console.log(filterElement)
+        const filterElement = createFilterElement(key, filters[key].unit, filters[key].value, filters[key].min, filters[key].max)
+        // console.log(filterElement)
 
-    filtersContainer.appendChild(filterElement);
+        filtersContainer.appendChild(filterElement);
 
+    })    
+}
+
+createFilters();
+
+imageInput.addEventListener("change", (e)=>{
+    const file = e.target.files[0];    
+    const imagePlaceholder = document.querySelector(".placeholder");
+    imageCanvas.style.display = "block";
+    imagePlaceholder.style.display = "none";
+
+    img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload = ()=>{
+        image = img;
+        imageCanvas.width = img.width;
+        imageCanvas.height = img.height;
+        canvasCtx.drawImage(img,0,0);
+    }
+    
+})
+
+function applyFilters(){
+       if (!image) {
+        if (!imageNotSelectedWarned) {
+            alert("Please choose an image before applying filters.");
+            imageNotSelectedWarned = true;
+        }
+        return;
+    }
+
+    imageNotSelectedWarned = false;
+
+
+    canvasCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+
+    canvasCtx.save();
+    canvasCtx.filter = `
+        brightness(${filters.brightness.value}${filters.brightness.unit})
+        contrast(${filters.contrast.value}${filters.contrast.unit})
+        saturate(${filters.saturation.value}${filters.saturation.unit})
+        hue-rotate(${filters.hueRotation.value}${filters.hueRotation.unit})
+        blur(${filters.blur.value}${filters.blur.unit})
+        grayscale(${filters.grayscale.value}${filters.grayscale.unit})
+        sepia(${filters.sepia.value}${filters.sepia.unit})
+        opacity(${filters.opacity.value}${filters.opacity.unit})
+        invert(${filters.invert.value}${filters.invert.unit})
+    `.trim();
+
+    canvasCtx.drawImage(image, 0, 0);
+    canvasCtx.restore();
+}
+
+
+// function applyFilters(){
+//     canvasCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+//     canvasCtx.filter = `
+//     brightness(${filters.brightness.value}${filters.brightness.unit})
+//     contrast(${filters.contrast.value}${filters.contrast.unit})
+//     saturate(${filters.saturation.value}${filters.saturation.unit})
+//     hue-rotate(${filters.hueRotation.value}${filters.hueRotation.unit})
+//     blur(${filters.blur.value}${filters.blur.unit})
+//     grayscale(${filters.grayscale.value}${filters.grayscale.unit})
+//     sepia(${filters.sepia.value}${filters.sepia.unit})
+//     opacity(${filters.opacity.value}${filters.opacity.unit})
+//     invert(${filters.invert.value}${filters.invert.unit})
+//     `.trim();
+//     canvasCtx.drawImage(image,0,0);
+// }
+
+resetBtn.addEventListener("click", (e)=>{
+        filters={
+        brightness : {
+            value : 100,
+            min : 0,
+            max : 200,
+            unit : "%"
+        },
+        contrast :  {
+            value : 100,
+            min : 0,
+            max : 200,
+            unit : "%"
+        },
+        saturation :  {
+            value : 100,
+            min : 0,
+            max : 200,
+            unit : "%"
+        },
+        hueRotation :  {
+            value : 0,
+            min : 0,
+            max : 360,
+            unit : "deg"
+        },
+        blur :  {
+            value : 0,
+            min : 0,
+            max : 20,
+            unit : "px"
+        },
+        grayscale : {
+            value : 0,
+            min : 0,
+            max : 100,
+            unit : "%"
+        },
+        sepia : { 
+            value : 0,
+            min : 0,
+            max : 100,
+            unit : "%"
+        },
+        opacity : {
+            value : 100,
+            min : 0,
+            max : 100,
+            unit : "%"
+        },
+        invert : {
+            value : 0,
+            min : 0,
+            max : 100,
+            unit : "%"
+        },
+    }
+    applyFilters();
+    filtersContainer.innerHTML = "";
+    createFilters();
+})
+
+downloadBtn.addEventListener("click",()=>{
+    const link = document.createElement("a");
+    link.download = "Edited-image.png";
+    link.href = imageCanvas.toDataURL();
+    link.click();
 })
